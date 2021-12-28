8daea79955bb6d556ce3ab41955e7987c1135d66
Robert Simai <rsimai@suse.com>
2021-12-28 12:14:08 +0100

    simple python demo code

--- arduino-serial.py
+++ arduino-serial.py
@@ -6,8 +6,11 @@ ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate = 9600
 
 while True:
-    cmd = input('Command: ')
+    cmd = input('Command (l,r,u,d,exit) : ')
+    if cmd == "exit":
+        exit()
     ser.write(cmd.encode())
+    ser.write(b'\n')
     ser.flush()
 
 ser.close()
